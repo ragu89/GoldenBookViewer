ebf06210e9f3ec5591e2eb61262b066b5bf03bd3
Guye, Raphael <raphael.guye@swatchgroup.com>
2017-04-14 19:45:41 +0200

    Refactoring AdService

--- GoldenBookViewer/AdService.m
+++ GoldenBookViewer/AdService.m
@@ -13,6 +13,8 @@
 
 @implementation AdService
 
+#pragma mark - Service methods
+
 -(void)synchronizeAdsWithCompletionHandler:(void (^)())completionHandler
 {
     [[RestClient sharedInstance] getAdsWithCompletionHandler:^(NSArray *ads, NSError *error) {
@@ -23,39 +25,6 @@
         
         [self downloadImagesForPhotos:ads withCompletionHandler:completionHandler];
     }];
-}
-
--(void)saveInPlistFile:(NSArray*)ads {
-    NSString *plistName = @"ads.json";
-    NSString *filepath = [[self getCacheFilepath] stringByAppendingString:[NSString stringWithFormat:@"/%@", plistName]];
-    NSFileManager *fm = [NSFileManager defaultManager];
-    
-    if([fm fileExistsAtPath:filepath]) {
-        [fm removeItemAtPath:filepath error:nil];
-    }
-    
-    NSMutableArray *adsForJson = [NSMutableArray new];
-    for(Ad *ad in ads) {
-        [adsForJson addObject:[ad convertToNSDictionary]];
-    }
-    
-    NSError *writeError;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:adsForJson options:NSJSONWritingPrettyPrinted error:&writeError];
-    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    
-    NSLog(@"Try to save json file in %@", filepath);
-    
-    writeError = nil;
-    BOOL isFileSaved = [jsonString writeToFile:filepath
-                                    atomically:YES
-                                      encoding:NSUTF8StringEncoding
-                                         error:&writeError];
-    
-    if(!isFileSaved) {
-        NSLog(@"KO - JSON file not correctly saved");
-    } else {
-        NSLog(@"OK - JSON file correctly saved");
-    }
 }
 
 -(void)downloadImagesForPhotos:(NSArray*)ads withCompletionHandler:(void (^)())completionHandler
@@ -161,6 +130,8 @@
     }
 }
 
+#pragma mark - Private methods
+
 -(BOOL)isPhotoMissing:(NSString*)photoId {
     NSString *filepath = [[self getCacheFilepath] stringByAppendingString:[NSString stringWithFormat:@"/%@", photoId]];
     
@@ -170,6 +141,39 @@
         return YES;
     } else {
         return NO;
+    }
+}
+
+-(void)saveInPlistFile:(NSArray*)ads {
+    NSString *plistName = @"ads.json";
+    NSString *filepath = [[self getCacheFilepath] stringByAppendingString:[NSString stringWithFormat:@"/%@", plistName]];
+    NSFileManager *fm = [NSFileManager defaultManager];
+    
+    if([fm fileExistsAtPath:filepath]) {
+        [fm removeItemAtPath:filepath error:nil];
+    }
+    
+    NSMutableArray *adsForJson = [NSMutableArray new];
+    for(Ad *ad in ads) {
+        [adsForJson addObject:[ad convertToNSDictionary]];
+    }
+    
+    NSError *writeError;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:adsForJson options:NSJSONWritingPrettyPrinted error:&writeError];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"Try to save json file in %@", filepath);
+    
+    writeError = nil;
+    BOOL isFileSaved = [jsonString writeToFile:filepath
+                                    atomically:YES
+                                      encoding:NSUTF8StringEncoding
+                                         error:&writeError];
+    
+    if(!isFileSaved) {
+        NSLog(@"KO - JSON file not correctly saved");
+    } else {
+        NSLog(@"OK - JSON file correctly saved");
     }
 }
 
